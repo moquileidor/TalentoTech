9c7f23fe643d069a17ad19cbeaedbfe4167e6f05
Jorge Alvarado <jalvaradocuesta@gmail.com>
2025-02-22 11:50:16 -0500

    Ejercicio 38,39

--- src/Condicionales/Ejercicio38.java
+++ src/Condicionales/Ejercicio38.java
@@ -1,4 +1,20 @@
 package Condicionales;
 
+import java.util.Scanner;
+
 public class Ejercicio38 {
+    public void run(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el numero de materias a cursar: ");
+        int materias = sc.nextInt();
+        int valorMateria = 350000;
+
+        int total = materias * valorMateria;
+
+        System.out.println("El valor de cada materia es: " + valorMateria);
+        System.out.println("El valor total a pagar es: " + total);
+
+
+    }
 }
